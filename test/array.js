d76b66eb6b3c0f38750bcd495aefa31a374ae074
jiubao <xia.ldj@gmail.com>
2017-06-05 11:16:51 +0800

    flatten

--- test/array.js
+++ test/array.js
@@ -1,9 +1,10 @@
 var should = require('chai').should();
 var assert = require('chai').assert;
+var _ = require('lodash');
 
 var array = require('../lib/array');
 
-describe('Test remove array items', function() {
+describe('Remove array item by index', function() {
 
   var arr = [];
 
@@ -32,118 +33,25 @@ describe('Test remove array items', function() {
   });
 });
 
-/*
+describe('Flatten the array', function() {
+  var arr = [];
 
-describe('Test naive dependency injection', function() {
+  beforeEach(function() {});
 
-  var injector;
-  beforeEach(function() {
-    injector = createInjector();
-  });
-
-  it('should inject literal object', function() {
-    injector.inject('book', {pageNumber: 10});
-    var book = injector.get('book');
-    // book.should.be.a('object');
-    assert.equal(book.pageNumber, 10);
-    // assert.equal(injector.get('book').pageNumber, 10);
-  });
-
-  it('should inject by factory function', function() {
-    injector.inject('car', function() {
-      return {
-        run: function() {
-          return 'running';
-        },
-        brake: function() {
-          return 'braking';
-        }
-      }
-    });
-    var car = injector.get('car');
-    assert.equal(car.run(), 'running');
-  });
-
-  it('should inject dependencies in array', function() {
-    injector.inject('car', ['wheel', 'engine', function(wheel, engine) {
-      return {
-        description: function() {
-          return 'bmw';
-        }
-      }
-    }]);
-
-    var car = injector.get('car');
-    car.should.be.a('object');
-
-  });
-
-  it('should inject dependencies', function() {
-
-    injector.inject('wheel', {
-      count: 4
-    });
-
-    injector.inject('engine', {
-      brand: 'oooo'
-    });
-
-    injector.inject('car', ['wheel', 'engine', function(wheel, engine) {
-      return {
-        description: function() {
-          return wheel.count + ' wheels, ' + engine.brand + ' engine';
-        }
-      }
-    }]);
-
-    var car = injector.get('car');
-
-    console.log(car.description());
-    car.description().should.be.a('string');
-
-
-  });
-
-  it('should inject by reflection', function() {
-
-    injector.inject('wheel', {
-      count: 4
-    });
-
-    injector.inject('engine', {
-      brand: 'oooo'
-    });
-
-    injector.inject('car', function(wheel, engine) {
-      return {
-        description: function() {
-          return wheel.count + ' wheels, ' + engine.brand + ' engine';
-        }
-      }
-    });
-
-  });
-
-  it('should inject by provider', function() {
-
-    injector.inject('car', ['wheel', 'engine', function(wheel, engine) {
-      return {
-        description: function() {
-          return wheel.count + ' wheels, ' + engine.brand + ' engine';
-        }
-      }
-    }]);
-
-    injector.inject('wheel', {
-      count: 4
-    });
-
-    injector.inject('engine', {
-      brand: 'oooo'
-    });
-
+  it('basic test', function() {
+    var arr = [
+      1,
+      [
+        2,
+        [
+          3,
+          4
+        ],
+        5
+      ],
+      6
+    ];
+    assert.deepEqual(array.flatten(arr), _.flattenDeep(arr));
   });
 
 });
-
-*/
